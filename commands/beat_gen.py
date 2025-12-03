d062504d8b204ad0ee73a316deef5346a5ab84bb
xelphyr <xelphyrnull@gmail.com>
2025-12-03 10:10:13 +0530

    finished beat_generation() in commands/beat_gen.py

--- commands/beat_gen.py
+++ commands/beat_gen.py
@@ -15,6 +15,13 @@ def beat_generation():
     user_id = data.get("user_id")
     text = data.get("text")
     if text is None or text == "":
-        client.chat_postEphemeral(channel=channel_id, user=user_id, text="Format of the command: `/beat-gen <----------------> <grouping> <bpm>`")
-        client.chat_postEphemeral(channel=channel_id, user=user_id, text="Each dash represents a beat that you generate from ")
+        client.chat_postEphemeral(channel=channel_id, user=user_id, text="Format of the command: `/beat-gen <----------------> <grouping> <bpm> <type>`")
+        client.chat_postEphemeral(channel=channel_id, user=user_id, text="`<---->`: Each dash represents a beat that you can add. For example: `x-x-`, or `---x---x---x---x`, or `x---x---x-x-`. You can imagine a metronome, and at each beat you move to the next dash. If the dash is actually an X, you play it out.")
+        client.chat_postEphemeral(channel=channel_id, user=user_id, text="`<grouping>`: This is how many dashes to play per beat. If the grouping were two and the bpm were 120, you'd have 4 notes every second, for example.")
+        client.chat_postEphemeral(channel=channel_id, user=user_id, text="`<bpm>`: The number of beats per minute. See <grouping> for more context.")
+        client.chat_postEphemeral(channel=channel_id, user=user_id, text="`<type>`: The bot supports `line` for now.")
+    else:
+        Thread(target=process_beat_gen, args=(data,)).start()
     return Response(), 200
+
+def process_beat_gen(data):
